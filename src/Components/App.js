a949d2d88a251d8d4ac6d8c9692d93b5ea701ff4
neotmons <neotmons@gmail.com>
2019-05-22 23:28:10 +0900

    #4.1. GlobalStyles and Theme

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import {ThemeProvider} from "styled-components";
+import Theme from "../Style/Theme";
+import GlobalStyles from "../Style/GlobalStyles";
 
-function App() {
-  return (
-    <div className="App" />
-  );
-}
 
-export default App;
+
+export default () => (
+  <ThemeProvider theme={Theme}>
+    <GlobalStyles />
+  </ThemeProvider>
+);
